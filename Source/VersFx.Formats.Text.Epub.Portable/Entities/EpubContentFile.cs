ebc07b453a91670acc08e71871e375e483c864b5
Leonardo Ribeiro da Silva <leonardo.silva@digitalpages.com.br>
2017-01-13 14:21:59 -0200

    feat(EpubContentFile): adicionando informação referente ao tamanho do arquivo do epub

--- Source/VersFx.Formats.Text.Epub.Portable/Entities/EpubContentFile.cs
+++ Source/VersFx.Formats.Text.Epub.Portable/Entities/EpubContentFile.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.IO;
-using System.IO.Compression;
 using System.Threading.Tasks;
-using ICSharpCode.SharpZipLib.Zip;
 
 namespace VersFx.Formats.Text.Epub.Entities
 {
@@ -10,49 +8,49 @@ namespace VersFx.Formats.Text.Epub.Entities
 	{
 		private WeakReference<EpubBook> book { get; set; }
 		public string FileName { get; private set; }
-        public string ZipEntry { get; set; }
+		public string ZipEntry { get; set; }
 		public EpubContentType ContentType { get; private set; }
 		public string ContentMimeType { get; private set; }
+		public long ContentSize { get; private set; }
 
 		public EpubContentFile(string fileName, EpubContentType type, string mime, string zipEntry, EpubBook book)
 		{
-            this.book = new WeakReference<EpubBook>(book);
-		    this.ZipEntry = zipEntry;
+			this.book = new WeakReference<EpubBook>(book);
+			this.ZipEntry = zipEntry;
 			this.FileName = fileName;
 			this.ContentType = type;
 			this.ContentMimeType = mime;
 		}
 
-		public Task<Stream> Resolve()
+		public async Task Prepare()
 		{
-//            EpubBook bookref;
-//            Stream stream = null;
-//
-//            if (book.TryGetTarget(out bookref) && bookref.Zip != null)
-//            {
-//                stream = await bookref.Zip.ResolveEntry(ZipEntry);
-//            }
-//
-//		    return stream;
+			EpubBook bookref;
 
-            return Task.Run( async () =>
-		    {
-		        EpubBook bookref;
-		        Stream stream = null;
+			if (book.TryGetTarget(out bookref) && bookref.Zip != null)
+			{
+				var entry = await bookref.Zip.Entry(ZipEntry).ConfigureAwait(false);
 
-		        if (book.TryGetTarget(out bookref) && bookref.Zip != null)
-		        {
-		            stream = await bookref.Zip.ResolveEntry(ZipEntry);
-		        }
-
-		        return stream;
-		    });
+				ContentSize = entry.Size;
+			}
 		}
 
-	    public void Dispose()
-	    {
-	        book?.SetTarget(null);
-	        book = null;
-	    }
+		public async Task<Stream> Resolve()
+		{
+			EpubBook bookref;
+			Stream stream = null;
+
+			if (book.TryGetTarget(out bookref) && bookref.Zip != null)
+			{
+				stream = await bookref.Zip.ResolveEntry(ZipEntry).ConfigureAwait(false);
+			}
+
+			return stream;
+		}
+
+		public void Dispose()
+		{
+			book?.SetTarget(null);
+			book = null;
+		}
 	}
 }
